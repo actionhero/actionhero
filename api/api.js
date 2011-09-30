4ec27c6939b3fcce78a24139973ce90718bb475a
Evan Tahler <evantahler@gmail.com>
2011-09-30 16:26:41 -0400

    cleanup `

--- api/api.js
+++ api/api.js
@@ -1,7 +1,7 @@
 var api = api = api || {}; // the main namespace for future methods
 
 ////////////////////////////////////////////////////////////////////////////
-// Includes
+// Global Includes
 
 api.sys = require("sys"),
 api.http = require("http"),
@@ -13,14 +13,11 @@ api.fs = require("fs");
 // Init
 var app = require('express').createServer();
 api.configData = JSON.parse(api.fs.readFileSync('config.json','utf8')); 
-
-////////////////////////////////////////////////////////////////////////////
-// Required functions
 api.utils = require("./utils.js").utils; // api.log() method
 api.log = require("./logger.js").log; // api.log() method
 try { api.fs.mkdirSync(api.configData.logFolder, "777") } catch(e) {} // ensure the logging directory exists
 
-console.log("Server Started");
+console.log("*** Server Started @ " + api.utils.sqlDateTime() + " ***");
 app.listen(api.configData.serverPort);
 
 app.get('/', function(req, res){
