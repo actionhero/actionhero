96c8af5b9ae39b8eba56807e2f7eaed78b775c04
Evan Tahler <evantahler@gmail.com>
2011-10-02 20:08:58 -0400

    basic actions are working along with action segmentation!

--- api/api.js
+++ api/api.js
@@ -3,8 +3,9 @@
 // Evan Tahler @ Fall 2011
 
 var api = api = api || {}; // the api namespace.  Everything uses this.
+
 ////////////////////////////////////////////////////////////////////////////
-// Global Includes
+// Init
 
 api.sys = require("sys"),
 api.http = require("http"),
@@ -12,8 +13,6 @@ api.url = require("url"),
 api.path = require("path"),
 api.fs = require("fs");
 
-////////////////////////////////////////////////////////////////////////////
-// Init
 api.expressServer = require('express')
 api.app = api.expressServer.createServer();
 api.app.use(api.expressServer.cookieParser());
@@ -38,9 +37,11 @@ api.postVariables = api.configData.postVariables || [];
 ////////////////////////////////////////////////////////////////////////////
 // populate actions
 api.actions = {};
+api.actionsArray = [];
 api.fs.readdirSync("./actions").forEach( function(file) {
 	var actionName = file.split(".")[0];
 	api.actions[actionName] = require("./actions/" + file)[actionName];
+	api.actionsArray.push(actionName);
 	api.log("action loaded: " + actionName);
 });
 
@@ -56,9 +57,10 @@ if (api.configData.cronProcess)
 ////////////////////////////////////////////////////////////////////////////
 // Request Processing
 api.app.get('/', function(req, res, next){
-	console.log(req.cookies);
 	api.timer = {};
 	api.timer.startTime = new Date().getTime();
+	api.response = {}; // the data returned from the API
+	api.error = false; 	// errors and requst state
 	
 	//params & cookies
 	api.params = {};
@@ -66,15 +68,26 @@ api.app.get('/', function(req, res, next){
 		api.params[postVar] = req.param(postVar);
 		if (api.params[postVar] === undefined){ api.params[postVar] = req.cookies[postVar]; }
 	});
-	console.log(api.params);
-	
-	// errors and requst state
-	api.error = false;
-	
-	api.response = {}; // the data returned from the API
-	
+		
 	// process
-	api.response.random = Math.random();
+	api.action = undefined;
+	if(api.params["action"] == undefined)
+	{
+		api.error = "You must provide an action. Use action=describeActions to see a list."
+	}
+	else
+	{
+		if(api.actions[api.params["action"]] != undefined)
+		{
+			api.action = api.params["action"];
+			api.actions[api.action](api);
+		}
+		else
+		{
+			api.error = "That is not a known action. Use action=describeActions to see a list."
+		}
+	}
 	
+	// response
   	res.send(api.build_response(res));
 });
