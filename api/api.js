1445c81abaee50e93c451ec67a1ddee3ee51ec03
Evan Tahler <evantahler@gmail.com>
2011-10-04 00:28:20 -0700

    "DB models and logging to the DB via model"

--- api/api.js
+++ api/api.js
@@ -3,103 +3,172 @@
 // Evan Tahler @ Fall 2011
 
 ////////////////////////////////////////////////////////////////////////////
+// Init
+function initRequires(api, next)
+{
+	// run me serially
+	api.sys = require("sys"),
+	api.http = require("http"),
+	api.url = require("url"),
+	api.path = require("path"),
+	api.fs = require("fs");
+	api.SequelizeBase = require("sequelize");
+	api.expressServer = require('express');
+	api.async = require('async');
+	api.app = api.expressServer.createServer();
+	api.app.use(api.expressServer.cookieParser());
+	api.configData = JSON.parse(api.fs.readFileSync('config.json','utf8')); 
+
+	api.utils = require("./utils.js").utils;
+	api.log = require("./logger.js").log;
+	api.tasks = require("./tasks.js").tasks;
+	for(var task in api.tasks){if (task != "Task"){api.log("task loaded: "+task)}}
+	api.build_response = require("./response.js").build_response; 
+
+	// ensure the logging directory exists
+	try { api.fs.mkdirSync(api.configData.logFolder, "777") } catch(e) {}; 
+	api.app.listen(api.configData.serverPort);
+	api.log("needed modules all included OK");
+	next(api);
+}
+
+////////////////////////////////////////////////////////////////////////////
+// DB setup
+function initDB(api)
+{
+	api.dbObj = new api.SequelizeBase(api.configData.database.database, api.configData.database.username, api.configData.database.password, {
+		host: api.configData.database.host,
+		port: api.configData.database.port,
+		logging: api.configData.database.consoleLogging
+	});
+
+	api.models = {};
+	api.modelsArray = [];
+	api.fs.readdirSync("./models").forEach( function(file) {
+		var modelName = file.split(".")[0];
+		api.models[modelName] = require("./models/" + file)['defineModel'](api);
+		api.modelsArray.push(modelName); 
+		api.log("model loaded: " + modelName);
+	});
+	api.dbObj.sync().on('success', function() {
+		api.log("DB conneciton sucessfull and Objects mapped to DB tables");
+	}).on('failure', function(error) {
+		api.log("trouble synchronizing models and DB.  Correct DB credentials?");
+		api.log(JSON.stringify(error));
+		api.log("exiting");
+		process.exit(1);
+	})
+}
+
+////////////////////////////////////////////////////////////////////////////
+// postVariable config and load
+function initPostVariables(api)
+{
+	api.postVariables = api.configData.postVariables || [];
+}
+
+////////////////////////////////////////////////////////////////////////////
+// populate actions
+function initActions(api)
+{
+	api.actions = {};
+	api.actionsArray = [];
+	api.fs.readdirSync("./actions").forEach( function(file) {
+		var actionName = file.split(".")[0];
+		api.actions[actionName] = require("./actions/" + file)[actionName];
+		api.actionsArray.push(actionName);
+		api.log("action loaded: " + actionName);
+	});
+}
+
+////////////////////////////////////////////////////////////////////////////
+// Periodic Tasks (fixed timer events)
+function initCron(api)
+{
+	if (api.configData.cronProcess)
+	{
+		api.processCron = require("./cron.js").processCron;
+		api.cronTimer = setTimeout(api.processCron, api.configData.cronTimeInterval, api);
+		api.log("periodic (internal cron) interval set to process evey " + api.configData.cronTimeInterval + "ms");
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////
+// Request Processing
+function initListen(api)
+{
+	api.app.get('/', function(req, res, next){
+		api.timer = {};
+		api.timer.startTime = new Date().getTime();
+		api.response = {}; // the data returned from the API
+		api.error = false; 	// errors and requst state
+	
+		//params & cookies
+		api.params = {};
+		api.postVariables.forEach(function(postVar){
+			api.params[postVar] = req.param(postVar);
+			if (api.params[postVar] === undefined){ api.params[postVar] = req.cookies[postVar]; }
+		});
+	
+		if(api.configData.logRequests){api.log("request from " + req.connection.remoteAddress + " | params: " + JSON.stringify(api.params));}
+		
+		// process
+		api.action = undefined;
+		if(api.params["action"] == undefined)
+		{
+			api.error = "You must provide an action. Use action=describeActions to see a list."
+		}
+		else
+		{
+			if(api.actions[api.params["action"]] != undefined)
+			{
+				api.action = api.params["action"];
+				api.actions[api.action](api);
+			}
+			else
+			{
+				api.error = "That is not a known action. Use action=describeActions to see a list."
+			}
+		}
+	
+		// response
+		var response = api.build_response(res);
+	  	res.send(response);
+		if(api.configData.logRequests){api.log("request from " + req.connection.remoteAddress + " | response: " + JSON.stringify(response));}
+		var logRecord = api.models.log.build({
+			ip: req.connection.remoteAddress,
+			action: api.action,
+			error: api.error,
+			params: JSON.stringify(api.params)
+		});
+		logRecord.save();
+	});
+}
+
+////////////////////////////////////////////////////////////////////////////
+// final flag
+function initComplete(api)
+{
+	api.log("*** Server Started @ " + api.utils.sqlDateTime() + " @ port " + api.configData.serverPort + " ***");
+}
+
+////////////////////////////////////////////////////////////////////////////
+// GO!
+
 // Force NPM to be update... you probably don't want this in production
 // exec = require('child_process').exec
 // exec("npm update");
 
-////////////////////////////////////////////////////////////////////////////
-// Init
-
 var api = api = api || {}; // the api namespace.  Everything uses this.
 
-api.sys = require("sys"),
-api.http = require("http"),
-api.url = require("url"),
-api.path = require("path"),
-api.fs = require("fs");
-
-api.expressServer = require('express')
-api.app = api.expressServer.createServer();
-api.app.use(api.expressServer.cookieParser());
-api.configData = JSON.parse(api.fs.readFileSync('config.json','utf8')); 
-
-api.utils = require("./utils.js").utils;
-api.log = require("./logger.js").log;
-api.tasks = require("./tasks.js").tasks;
-for(var task in api.tasks){if (task != "Task"){api.log("task loaded: "+task)}}
-api.build_response = require("./response.js").build_response; 
-
-// ensure the logging directory exists
-try { api.fs.mkdirSync(api.configData.logFolder, "777") } catch(e) {}; 
-api.app.listen(api.configData.serverPort);
-
-////////////////////////////////////////////////////////////////////////////
-// DB setup
-
-////////////////////////////////////////////////////////////////////////////
-// postVariable config and load
-api.postVariables = api.configData.postVariables || [];
-
-////////////////////////////////////////////////////////////////////////////
-// populate actions & Tasks
-api.actions = {};
-api.actionsArray = [];
-api.fs.readdirSync("./actions").forEach( function(file) {
-	var actionName = file.split(".")[0];
-	api.actions[actionName] = require("./actions/" + file)[actionName];
-	api.actionsArray.push(actionName);
-	api.log("action loaded: " + actionName);
+initRequires(api, function(api) {
+	api.async.series([
+		initDB(api),
+		initPostVariables(api),
+		initActions(api),
+		initCron(api),
+		initListen(api),
+		initComplete(api),
+	]);
 });
 
-////////////////////////////////////////////////////////////////////////////
-// Periodic Tasks (fixed timer events)
-if (api.configData.cronProcess)
-{
-	api.processCron = require("./cron.js").processCron;
-	api.cronTimer = setTimeout(api.processCron, api.configData.cronTimeInterval, api);
-	api.log("periodic (internal cron) interval set to process evey " + api.configData.cronTimeInterval + "ms");
-}
-
-api.log("*** Server Started @ " + api.utils.sqlDateTime() + " @ port " + api.configData.serverPort + " ***");
-
-////////////////////////////////////////////////////////////////////////////
-// Request Processing
-api.app.get('/', function(req, res, next){
-	api.timer = {};
-	api.timer.startTime = new Date().getTime();
-	api.response = {}; // the data returned from the API
-	api.error = false; 	// errors and requst state
-	
-	//params & cookies
-	api.params = {};
-	api.postVariables.forEach(function(postVar){
-		api.params[postVar] = req.param(postVar);
-		if (api.params[postVar] === undefined){ api.params[postVar] = req.cookies[postVar]; }
-	});
-	
-	if(api.configData.logRequests){api.log("request from " + req.connection.remoteAddress + " | params: " + JSON.stringify(api.params));}
-		
-	// process
-	api.action = undefined;
-	if(api.params["action"] == undefined)
-	{
-		api.error = "You must provide an action. Use action=describeActions to see a list."
-	}
-	else
-	{
-		if(api.actions[api.params["action"]] != undefined)
-		{
-			api.action = api.params["action"];
-			api.actions[api.action](api);
-		}
-		else
-		{
-			api.error = "That is not a known action. Use action=describeActions to see a list."
-		}
-	}
-	
-	// response
-	var response = api.build_response(res);
-  	res.send(response);
-	if(api.configData.logRequests){api.log("request from " + req.connection.remoteAddress + " | response: " + JSON.stringify(response));}
-});
