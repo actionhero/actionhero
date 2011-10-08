105ccf206fc2df121ca4c822cac08bff46dab47c
Evan Tahler <evantahler@gmail.com>
2011-10-08 16:15:45 -0700

    Edited api/utils.js via GitHub

--- api/utils.js
+++ api/utils.js
@@ -54,5 +54,20 @@ utils.requiredParamChecker = function(api, required_params)
 }
 
 ////////////////////////////////////////////////////////////////////////////
+// generate a random string
+utils.randomString = function(bits)
+{
+	var chars,rand,i,ret
+	chars='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+	ret=''
+  	while(bits > 0)
+	{
+	   rand=Math.floor(Math.random()*0x100000000) // 32-bit integer
+	   for(i=26; i>0 && bits>0; i-=6, bits-=6) ret+=chars[0x3F & rand >>> i]
+	}
+	return ret
+}
+
+////////////////////////////////////////////////////////////////////////////
 // Request Processing
 exports.utils = utils;
