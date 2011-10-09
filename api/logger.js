ea7a4157258260246f00069194aeba9b9cf12242
Evan Tahler <evantahler@gmail.com>
2011-10-09 13:04:50 -0700

    fixing logger I/O errors

--- api/logger.js
+++ api/logger.js
@@ -5,7 +5,11 @@ function log(original_message)
 	console.log(message);
 	if(this.configData.logging)
 	{
-		this.fs.createWriteStream((this.configData.logFolder + "/" + this.configData.logFile), {flags:"a"}).write(message + "\r\n");
+		try{
+			this.fs.createWriteStream((this.configData.logFolder + "/" + this.configData.logFile), {flags:"a"}).write(message + "\r\n");
+		}catch(e){
+			console.log(" !!! Error writing to log file: " + e);
+		}
 	}
 };
 
