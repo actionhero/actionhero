4e337f6cfdf08e7e5ddb8abeb80dd00880886620
Evan Tahler <evantahler@gmail.com>
2011-10-10 00:42:25 -0700

    Fixing the logger.  It opened way to many streams... now only one!

--- api/logger.js
+++ api/logger.js
@@ -1,12 +1,17 @@
 function log(original_message)
-{
+{	
 	var d = new Date();
 	message = this.utils.sqlDateTime() + " | " + original_message;
 	console.log(message);
 	if(this.configData.logging)
 	{
+		if (this.logWriter == null)
+		{
+			this.logWriter = this.fs.createWriteStream((this.configData.logFolder + "/" + this.configData.logFile), {flags:"a"});
+		}
+		
 		try{
-			this.fs.createWriteStream((this.configData.logFolder + "/" + this.configData.logFile), {flags:"a"}).write(message + "\r\n");
+			this.logWriter.write(message + "\r\n");
 		}catch(e){
 			console.log(" !!! Error writing to log file: " + e);
 		}
