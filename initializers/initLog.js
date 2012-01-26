a84fc6eaa008a29ef8c75bfffe1cdc7f4a2907c6
Evan Tahler <evantahler@gmail.com>
2012-01-26 09:41:18 -0800

    reverting logging to fire inline (you need to see errors as they happen)

--- initializers/initLog.js
+++ initializers/initLog.js
@@ -41,7 +41,7 @@ var initLog = function(api, next){
 				else if(style == "zalgo"){inner_message = api.consoleColors.zalgo(inner_message);}
 			}
 			console_message += inner_message;
-			process.nextTick(function() { console.log(console_message); });
+			console.log(console_message);
 			var file_message = time_string + " | " + original_message;
 			if (api.logWriter == null){
 				api.logWriter = api.fs.createWriteStream((api.configData.logFolder + "/" + api.configData.logFile), {flags:"a"});
