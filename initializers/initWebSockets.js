ccdcc4bd6bd49c447cf6b55a2f6ce41e98486672
Evan Tahler <evan@taskrabbit.com>
2012-07-22 16:53:57 -0700

    mystery solved with 2 socket listners

--- initializers/initWebSockets.js
+++ initializers/initWebSockets.js
@@ -12,45 +12,59 @@ var initWebSockets = function(api, next)
 			api.log( "(socket.io) " + original_message, ["red", "bold"]);
 		},
 		warn: function(original_message){
-			api.log( "(socket.io) " + original_message, "red");
+			// api.log( "(socket.io) " + original_message, "red");
 		},
 		info: function(original_message){
-			api.log( "(socket.io) " + original_message);
+			// api.log( "(socket.io) " + original_message);
 		},
 		debug: function(original_message){
-			api.log( "(socket.io) " + original_message, "grey");
+			// api.log( "(socket.io) " + original_message, "grey");
 		}
 	};
 
 	var io_http = api.io.listen(api.webServer.webApp);
 	IOs.push(io_http);
-	if(api.configData.secureWebServer.enable){
-		var io_https = api.io.listen(api.webServer.secureWebApp);
-		IOs.push(io_https);
-	}
-
-	var c = api.configData.redis;
-	if(c.enable == true){
-		var RedisStore = require('socket.io/lib/stores/redis');
-		var pub    = api.redisPackage.createClient(c.port, c.host, c.options);
-		var sub    = api.redisPackage.createClient(c.port, c.host, c.options);
-		var client = api.redisPackage.createClient(c.port, c.host, c.options);
-
-		if(c.password != null){ 
-			pub.auth(c.password, function(){}); 
-			sub.auth(c.password, function(){}); 
-			client.auth(c.password, function(){}); 
-		}
-	}
+	// TODO: Having 2 of these open at the same time crashes when redis is in use
+	// if(api.configData.secureWebServer.enable){
+	// 	var io_https = api.io.listen(api.webServer.secureWebApp);
+	// 	IOs.push(io_https);
+	// }
 
 	for(var i in IOs){
 		var io = IOs[i];
 
-		io.set('log level', 3);
+		io.set('log level', 0);
 		io.enable('browser client minification'); 
 		io.enable('browser client etag');
 		io.enable('browser client gzip');
-		io.set('logger', logger);
+
+		var c = api.configData.redis;
+		if(c.enable == true){
+			var RedisStore = require('socket.io/lib/stores/redis');
+			var pub    = api.redisPackage.createClient(c.port, c.host, c.options);
+			var sub    = api.redisPackage.createClient(c.port, c.host, c.options);
+			var client = api.redisPackage.createClient(c.port, c.host, c.options);
+
+			pub.on("error", function(msg){ logger.error(msg); })
+			sub.on("error", function(msg){ logger.error(msg); })
+			client.on("error", function(msg){ logger.error(msg); })
+
+			if(c.password != null){ 
+				pub.auth(c.password, function(){
+					pub.select(c.DB, function(err,res){});
+				}); 
+				sub.auth(c.password, function(){
+					sub.select(c.DB, function(err,res){});
+				}); 
+				client.auth(c.password, function(){
+					client.select(c.DB, function(err,res){});
+				}); 
+			}else{
+				pub.select(c.DB, function(err,res){});
+				sub.select(c.DB, function(err,res){});
+				client.select(c.DB, function(err,res){});
+			}
+		}
 
 		if(c.enable == true){
 			io.set('store', new RedisStore({
@@ -80,19 +94,17 @@ var initWebSockets = function(api, next)
 			var welcomeMessage = {welcome: api.configData.socketServerWelcomeMessage, room: connection.room, context: "api"};
 	    	connection.emit('welcome', welcomeMessage);
 
-	    	connection.on('exit', function(){ connection.disconnect(); });
-	    	connection.on('quit', function(){ connection.disconnect(); });
-	    	connection.on('close', function(){ connection.disconnect(); });
+	    	connection.on('exit', function(data){ connection.disconnect(); });
+	    	connection.on('quit', function(data){ connection.disconnect(); });
+	    	connection.on('close', function(data){ connection.disconnect(); });
 	    	
-	    	connection.on('roomView', function(){});
-	    	connection.on('roomChange', function(){});
-	    	connection.on('say', function(){});
-	    	connection.on('action', function(){});
-
-			// socket.emit('news', { hello: 'world' });
-		    //  socket.on('my other event', function (data) {
-		    //    console.log(data);
-		    //  });
+	    	connection.on('test', function(data){
+	    		console.log(data)
+	    	});
+	    	connection.on('roomView', function(data){});
+	    	connection.on('roomChange', function(data){});
+	    	connection.on('say', function(data){});
+	    	connection.on('action', function(data){});
 
 			connection.on('disconnect', function(){
 				// TODO: This never gets called?
