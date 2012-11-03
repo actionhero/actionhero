070693e37948528868993695d1ff07861fc15966
Evan Tahler <evantahler@gmail.com>
2012-11-02 23:13:23 -0700

    refactor runTask action to use new connection builder

--- tasks/runAction.js
+++ tasks/runAction.js
@@ -5,21 +5,15 @@ var task = {};
 task.name = "runAction";
 task.description = "I will run an action and return the connection object";
 task.scope = "any";
-task.frequency = 0;
+task.frequency = 1000;
 
 /////////////////////////////////////////////////////////////////////
 // functional
 task.run = function(api, params, next){
 	if(params == null){params = {};}
-	
-	var connection = {
-		type: "task",
-		params: params,
-		error: false,
-		remoteIP: "task",
-		action: params.action,
-		response: {}
-	};
+
+	connection = api.utils.setupConnection(api, {id: 0}, 'task', 0, 0);
+	connection.params = params;
 	
 	api.processAction(api, connection, null, function(connection, cont){
 		if(connection.error){
