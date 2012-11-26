d2c75468d8d7ec238d3a44868a514d629ecd3bdb
Evan Tahler <evan@taskrabbit.com>
2012-11-25 21:25:53 -0500

    move help to include

--- bin/include/help.js
+++ bin/include/help.js
@@ -1,5 +1,5 @@
 exports['help'] = function(api, next){
-  var help = api.fs.readFileSync(process.cwd() + "/bin/help.txt");
+  var help = api.fs.readFileSync(process.cwd() + "/bin/include/help.txt");
   api.log(help);
 
   next();
