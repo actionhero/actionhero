a986e36d3979b91669c1e4637c0e335bb001065e
Evan Tahler <evan@taskrabbit.com>
2012-11-25 23:20:32 -0500

    sorted out detection if in a project or in code

--- bin/include/generate.js
+++ bin/include/generate.js
@@ -32,23 +32,26 @@
   |- tasks\r\n\
   |-- (your tasks)\r\n\
   |\r\n\
+  | readme.md\r\n\
+  | routes.js\r\n\
   | config.js\r\n\
   | package.json (be sure to include 'actionHero':'x')\r\n\
   ";
 
-  documents.config_js = fs.readFileSync(project_root + "/node_modules/actionHero/config.js");
-  documents.routes_js = fs.readFileSync(project_root + "/node_modules/actionHero/routes.js");
+  documents.config_js = binary.fs.readFileSync(binary.project_root + "/node_modules/actionHero/config.js");
+  documents.routes_js = binary.fs.readFileSync(binary.project_root + "/node_modules/actionHero/routes.js");
 
-  documents.cert_pem = fs.readFileSync(project_root + "/node_modules/actionHero/certs/server-cert.pem");
-  documents.key_pem = fs.readFileSync(project_root + "/node_modules/actionHero/certs/server-key.pem");
+  documents.cert_pem = binary.fs.readFileSync(binary.project_root + "/node_modules/actionHero/certs/server-cert.pem");
+  documents.key_pem = binary.fs.readFileSync(binary.project_root + "/node_modules/actionHero/certs/server-key.pem");
 
-  documents.action_actions_view = fs.readFileSync(project_root + "/node_modules/actionHero/actions/actionsView.js");
-  documents.action_file = fs.readFileSync(project_root + "/node_modules/actionHero/actions/file.js");
-  documents.action_random_number = fs.readFileSync(project_root + "/node_modules/actionHero/actions/randomNumber.js");
-  documents.action_status = fs.readFileSync(project_root + "/node_modules/actionHero/actions/status.js");
-  documents.action_chat = fs.readFileSync(project_root + "/node_modules/actionHero/actions/chat.js");
+  documents.action_actions_view = binary.fs.readFileSync(binary.project_root + "/node_modules/actionHero/actions/actionsView.js");
+  documents.action_file = binary.fs.readFileSync(binary.project_root + "/node_modules/actionHero/actions/file.js");
+  documents.action_status = binary.fs.readFileSync(binary.project_root + "/node_modules/actionHero/actions/status.js");
+  documents.action_chat = binary.fs.readFileSync(binary.project_root + "/node_modules/actionHero/actions/chat.js");
 
-  documents.task_runAction = fs.readFileSync(project_root + "/node_modules/actionHero/tasks/runAction.js");
+  documents.task_runAction = binary.fs.readFileSync(binary.project_root + "/node_modules/actionHero/tasks/runAction.js");
+
+  documents.public_actionHeroWebSocket = binary.fs.readFileSync(binary.project_root + "/node_modules/actionHero/public/javascripts/actionHeroWebSocket.js");
 
   documents.package_json = "{\r\n\
     \"author\": \"YOU <YOU@example.com>\",\r\n\
@@ -70,9 +73,9 @@
     },\r\n\
     \"devDependencies\": {},\r\n\
     \"scripts\": {\r\n\
-      \"start\": \"./node_modules/actionHero/bin/actionHero start\"\r\n\
-      \"startCluster\": \"./node_modules/actionHero/bin/actionHero startCluster\"\r\n\
-      \"help\": \"./node_modules/actionHero/bin/actionHero help\"\r\n\
+      \"start\": \"node ./node_modules/actionHero/bin/actionHero start\",\r\n\
+      \"startCluster\": \"node ./node_modules/actionHero/bin/actionHero startCluster\",\r\n\
+      \"help\": \"node ./node_modules/actionHero/bin/actionHero help\"\r\n\
     }\r\n\
   }\r\n\
   ";
@@ -86,52 +89,47 @@
 
   //////// LOGIC ////////
 
-  binary.log("\r\n**********\r\n");
   binary.log("Generating a new actionHero project...");
 
   // test that actionHero is installed
-  dir_exists("node_modules", null, function(){
+  binary.utils.dir_exists("node_modules", null, function(){
   	binary.log(" ! node_modules doesn't exist.  `npm install actionHero` first".red);
   	process.exit();
   });
-  dir_exists("node_modules/actionHero", null, function(){
+  binary.utils.dir_exists("node_modules/actionHero", null, function(){
   	binary.log(" ! node_modules/actionHero doesn't exist.  `npm install actionHero` first".red);
   	process.exit();
   });
 
   // make directories
-  create_dir_safely("actions");
-  create_dir_safely("pids");
-  create_dir_safely("certs");
-  create_dir_safely("initializers");
-  create_dir_safely("log");
-  create_dir_safely("public");
-  create_dir_safely("tasks");
+  binary.utils.create_dir_safely("actions");
+  binary.utils.create_dir_safely("pids");
+  binary.utils.create_dir_safely("certs");
+  binary.utils.create_dir_safely("initializers");
+  binary.utils.create_dir_safely("log");
+  binary.utils.create_dir_safely("public");
+  binary.utils.create_dir_safely("public/javascripts");
+  binary.utils.create_dir_safely("tasks");
 
   // make files
-  create_file_safely('config.js', documents.config_js);
-  create_file_safely('routes.js', documents.routes_js);
-  create_file_safely('package.json', documents.package_json);
-  create_file_safely('/certs/server-cert.pem', documents.cert_pem);
-  create_file_safely('/certs/server-key.pem', documents.key_pem);
-  create_file_safely('/actions/actionsView.js', documents.action_actions_view);
-  create_file_safely('/actions/file.js', documents.action_file);
-  create_file_safely('/actions/randomNumber.js', documents.action_random_number);
-  create_file_safely('/actions/status.js', documents.action_status);
-  create_file_safely('/actions/chat.js', documents.action_chat);
-  create_file_safely('/tasks/runAction.js', documents.task_runAction);
-  create_file_safely('/public/index.html', documents.index_html);
-  create_file_safely('/readme.md', documents.readme_md);
+  binary.utils.create_file_safely('config.js', documents.config_js);
+  binary.utils.create_file_safely('routes.js', documents.routes_js);
+  binary.utils.create_file_safely('package.json', documents.package_json);
+  binary.utils.create_file_safely('certs/server-cert.pem', documents.cert_pem);
+  binary.utils.create_file_safely('certs/server-key.pem', documents.key_pem);
+  binary.utils.create_file_safely('actions/actionsView.js', documents.action_actions_view);
+  binary.utils.create_file_safely('actions/file.js', documents.action_file);
+  binary.utils.create_file_safely('actions/status.js', documents.action_status);
+  binary.utils.create_file_safely('actions/chat.js', documents.action_chat);
+  binary.utils.create_file_safely('tasks/runAction.js', documents.task_runAction);
+  binary.utils.create_file_safely('public/index.html', documents.index_html);
+  binary.utils.create_file_safely('public/javascripts/actionHeroWebSocket.js', documents.public_actionHeroWebSocket);
+  binary.utils.create_file_safely('readme.md', documents.readme_md);
 
   binary.log("");
-  binary.log("Generation Complete.  Your project directory should look like this:");
-  binary.log(documents.projectMap.blue)
-
+  binary.log("Generation Complete.  Your project directory should look like this:\r\n" + documents.projectMap.blue);
   binary.log("");
-
   binary.log("run `npm start` to start your server");
-
-  binary.log("\r\n**********\r\n");
 
   next();
 
--- bin/include/help.js
+++ bin/include/help.js
@@ -1,5 +1,5 @@
 exports['help'] = function(api, next){
-  var help = api.fs.readFileSync(process.cwd() + "/bin/include/help.txt");
+  var help = api.fs.readFileSync(__dirname + "/help.txt");
   api.log(help);
 
   next();
