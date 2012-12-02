b8b9aa827f30d8db5413d5436d631ddc6a5a0e73
Evan Tahler <evantahler@gmail.com>
2012-12-02 14:43:52 -0800

    help

--- bin/include/help.js
+++ bin/include/help.js
@@ -1,5 +1,5 @@
-exports['help'] = function(api, next){
-  var help = api.fs.readFileSync(binary.paths.actionHero_root + "/bin/include/help.txt");
-  api.log(help);
+exports['help'] = function(binary, next){
+  var help = binary.fs.readFileSync(binary.paths.actionHero_root + "/bin/include/help.txt");
+  binary.log(help);
   next();
 }
