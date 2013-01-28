3e626b6143436bc65b48255c04f0f3473bddbc60
Evan Tahler <evan@taskrabbit.com>
2013-01-28 14:10:47 -0800

    update logger for binary actions

--- bin/include/unknownInput.js
+++ bin/include/unknownInput.js
@@ -1,5 +1,5 @@
 exports['unknownInput'] = function(binary, next){
-  binary.log("`" + binary.mainAction + "` is not a known action", ["red", "bold"]);
-  binary.log("run `" + "actionHero help".green +  "` for more information");
+  binary.log("`" + binary.mainAction + "` is not a known action", "warning");
+  binary.log("run `actionHero help` for more information", "info");
   next();
 }
