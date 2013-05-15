99995093d42e0493bbad8b0fdb24f8397f953cf0
Evan Tahler <evan@taskrabbit.com>
2013-05-15 00:28:36 -0700

    commit simpler random number

--- actions/randomNumber.js
+++ actions/randomNumber.js
@@ -3,7 +3,7 @@ var action = {};
 /////////////////////////////////////////////////////////////////////
 // metadata
 action.name = "randomNumber";
-action.description = "I am an API method which will generate a random number.  Different HTTP verbs will multiply the answer";
+action.description = "I am an API method which will generate a random number";
 action.inputs = {
   "required" : [],
   "optional" : []
@@ -16,18 +16,7 @@ action.outputExample = {
 /////////////////////////////////////////////////////////////////////
 // functional
 action.run = function(api, connection, next){
-  if(connection.method == "GET" || connection.method == null){
-    connection.response.randomNumber = Math.random();
-  }
-  else if(connection.method == "PUT"){
-    connection.response.randomNumber = Math.random() * 10;
-  }
-  else if(connection.method == "POST"){
-    connection.response.randomNumber = Math.random() * 100;
-  }
-  else if(connection.method == "DELETE"){
-    connection.response.randomNumber = Math.random() * 1000;
-  }
+  connection.response.randomNumber = Math.random();
   next(connection, true);
 };
 
