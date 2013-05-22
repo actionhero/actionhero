7f76dda4f803a228c45bb84500bacf1b9f839b91
Evan Tahler <evan@taskrabbit.com>
2013-05-21 18:01:20 -0700

    v6.0.1

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ var configData = {};
 /////////////////////////
 
 configData.general = {
-  apiVersion: "6.0.0",
+  apiVersion: "6.0.1",
   serverName: "actionHero API",
   // id: "myActionHeroServer",                                    // id can be set here, or generated dynamically.  be sure that every server you run as a unique ID (which will happen when genrated dynamically)
   serverToken: "change-me",                                       // A unique token to your application which servers will use to authenticate to eachother
