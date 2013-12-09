03f3062d6ca66edc4cd8d4c320ea8d05082e11b7
Bryan Tong <contact@nullivex.com>
2013-12-09 13:27:20 -0700

    Changed gruntfile setup to help test with and without redis. Not sure why node 0.8 is failing nothing changed here should affect that.

--- gruntfile.js
+++ gruntfile.js
@@ -20,7 +20,10 @@ grunt.initConfig({
     options: {
       reporter: 'spec'
     },
-    test: {
+    fakeredis: {
+      src: 'test/*.js'
+    },
+    realredis: {
       src: 'test/*.js'
     }
   },
@@ -52,9 +55,9 @@ grunt.loadNpmTasks('grunt-env')
 grunt.loadNpmTasks('grunt-mocha-test')
 grunt.loadNpmTasks('grunt-project-update')
 
-grunt.registerTask('testFakeRedis',['env:fakeredis','mochaTest'])
-grunt.registerTask('testRealRedis',['env:realredis','mochaTest'])
-grunt.registerTask('test',['testFakeRedis','testRealRedis'])
+grunt.registerTask('testFakeRedis',['env:fakeredis','mochaTest:fakeredis'])
+grunt.registerTask('testRealRedis',['env:realredis','mochaTest:realredis'])
+grunt.registerTask('test',['testFakeRedis'])
 
 grunt.registerTask('publish',['clean:publish','uglify:publish'])
 grunt.registerTask('update',['projectUpdate'])
