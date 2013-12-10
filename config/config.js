68c1f9600ce577d78b1cff7a3bcab83af78570a7
Evan Tahler <evantahler@gmail.com>
2013-12-09 22:09:52 -0800

    socket server should default to off

--- config/config.js
+++ config/config.js
@@ -201,18 +201,18 @@ config.servers = {
     //  You can also set connection.rawConnection.responseHttpCode to specify a code per request.
     returnErrorCodes: false
   },
-  'socket' : {
-    // TCP or TLS?
-    secure: false,
-    // passed to tls.createServer if secure=true. Should contain SSL certificates
-    serverOptions: {},
-    // Port or Socket
-    port: 5000,
-    // which IP to listen on (use 0.0.0.0 for all)
-    bindIP: '0.0.0.0'
-  },
   'websocket' : {
   }
+  // 'socket' : {
+  //   // TCP or TLS?
+  //   secure: false,
+  //   // passed to tls.createServer if secure=true. Should contain SSL certificates
+  //   serverOptions: {},
+  //   // Port or Socket
+  //   port: 5000,
+  //   // which IP to listen on (use 0.0.0.0 for all)
+  //   bindIP: '0.0.0.0'
+  // },
 };
 
 //////////////////////////////////
