02a94575e5323d3e4a7006283ef5777bb033828e
TJKoury <TJ@bisonpc.net>
2013-12-17 12:42:01 -0500

    Fixed tests per new folder layout

--- test/core_staticFile.js
+++ test/core_staticFile.js
@@ -1,6 +1,7 @@
 describe('Core: Static File', function(){
   var specHelper = require(__dirname + '/_specHelper.js').specHelper;
   var apiObj = {};
+  var defaultPath = '/public/default';
   var should = require('should');
 
   before(function(done){
@@ -9,27 +10,27 @@ describe('Core: Static File', function(){
       apiObj = specHelper.cleanAPIObject(api);
       //TODO: Why does travis-ci require the connection to be seeded like this?
       // It works locally on OSX and Ubuntu
-      specHelper.apiTest.get('/public/' + 'simple.html', 0, {}, function(response, json){});
+      specHelper.apiTest.get(defaultPath + 'simple.html', 0, {}, function(response, json){});
       done();
     })
   });
 
   it('file: response is NOT json', function(done){
-    specHelper.apiTest.get('/public/' + 'someRandomFile', 0, {}, function(response, json){
+    specHelper.apiTest.get(defaultPath + 'someRandomFile', 0, {}, function(response, json){
       should.not.exist(json);
       done();
     });
   });
 
   it('file: 404 pages', function(done){
-    specHelper.apiTest.get('/public/' + 'someRandomFile', 0, {}, function(response, json){
+    specHelper.apiTest.get(defaultPath + 'someRandomFile', 0, {}, function(response, json){
       response.statusCode.should.equal(404)
       done();
     });
   });
 
   it('file: an HTML file', function(done){
-    specHelper.apiTest.get('/public/' + 'simple.html', 0, {}, function(response, json){
+    specHelper.apiTest.get(defaultPath + 'simple.html', 0, {}, function(response, json){
       response.statusCode.should.equal(200);
       response.body.should.equal('<h1>ActionHero</h1>\\nI am a flat file being served to you via the API from ./public/simple.html<br />');
       done();
@@ -37,7 +38,7 @@ describe('Core: Static File', function(){
   });
 
   it('file: ?filename should work like a path', function(done){
-    specHelper.apiTest.get('/public/' + '?file=simple.html', 0, {}, function(response, json){
+    specHelper.apiTest.get(defaultPath + '?file=simple.html', 0, {}, function(response, json){
       response.statusCode.should.equal(200);
       response.body.should.equal('<h1>ActionHero</h1>\\nI am a flat file being served to you via the API from ./public/simple.html<br />');
       done();
@@ -45,7 +46,7 @@ describe('Core: Static File', function(){
   });
 
   it('I should not see files outside of the public dir', function(done){
-    specHelper.apiTest.get('/public/' + '?file=../config.json', 0, {}, function(response, json){
+    specHelper.apiTest.get(defaultPath + '?file=../config.json', 0, {}, function(response, json){
       response.statusCode.should.equal(404);
       response.body.should.equal(apiObj.config.general.flatFileNotFoundMessage);
       done();
@@ -53,7 +54,7 @@ describe('Core: Static File', function(){
   });
 
   it('file: index page should be served when requesting a path', function(done){
-    specHelper.apiTest.get('/public/', 0, {}, function(response, json){
+    specHelper.apiTest.get(defaultPath, 0, {}, function(response, json){
       response.statusCode.should.equal(200);
       response.body.should.be.a.String;
       done();
@@ -61,14 +62,14 @@ describe('Core: Static File', function(){
   });
 
   it('file: sub paths should work', function(done){
-    specHelper.apiTest.get('/public/' + 'logo/actionHero.png', 0, {}, function(response, json){
+    specHelper.apiTest.get(defaultPath + 'logo/actionHero.png', 0, {}, function(response, json){
       response.statusCode.should.equal(200);
       done();
     });
   });
 
   it('file: binary files should also work', function(done){
-    specHelper.apiTest.get('/public/' + 'logo/actionHero.png', 0, {}, function(response, json){
+    specHelper.apiTest.get(defaultPath + 'logo/actionHero.png', 0, {}, function(response, json){
       response.statusCode.should.equal(200);
       response.body.length.should.be.within(136836, 136920);
       done();
