ba1d7b5fb02ea55cdc249bc45e05bfddbaecfdcd
TJKoury <TJ@bisonpc.net>
2013-12-17 13:22:18 -0500

    Ok, I suck at editing

--- test/core_staticFile.js
+++ test/core_staticFile.js
@@ -1,7 +1,7 @@
 describe('Core: Static File', function(){
   var specHelper = require(__dirname + '/_specHelper.js').specHelper;
   var apiObj = {};
-  var defaultPath = '/public/default';
+  var defaultPath = '/public/default/';
   var should = require('should');
 
   before(function(done){
