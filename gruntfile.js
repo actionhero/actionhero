6671ce3c947aa437ca63d36c8b2f0bb3386e9881
Evan Tahler <evan@taskrabbit.com>
2013-12-17 20:51:58 -0800

    fix list grunt task

--- gruntfile.js
+++ gruntfile.js
@@ -78,17 +78,20 @@ var init = function(fn){
 }
 
 grunt.registerTask('list','List your actions and metadata',function(){
+  var done = this.async()
   init(function(api){
-    api.actions.actions.forEach(function(collection){
-      grunt.log.writeln(collection)
-      api.actions.actions[collection].forEach(function(version){
-        var action = api.actions.actions[collection][version]
+    for(var actionName in api.actions.actions){
+      grunt.log.writeln(actionName)
+      var collection = api.actions.actions[actionName]
+      for(var version in collection){
+        var action = collection[version];
         grunt.log.writeln('  ' + 'version: ' + version)
         grunt.log.writeln('    ' + action.description)
         grunt.log.writeln('    ' + 'required inputs: ' + action.inputs.required.join(', '))
         grunt.log.writeln('    ' + 'optional inputs: ' + action.inputs.optional.join(', '))
-      })
-    })
+      }
+    }
+    done()
   })
 })
 
