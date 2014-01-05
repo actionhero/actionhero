a853ad2906444e2cc1ab12ae3c8fd8aece23abf4
Evan Tahler <evan@taskrabbit.com>
2014-01-04 20:28:05 -0800

    remove optional note

--- routes.js
+++ routes.js
@@ -4,7 +4,6 @@ routes.js
 For web clients (http and https) you can define an optional RESTful mapping to help route requests to actions.
 If the client doesn't specify and action in a param, and the base route isn't a named action, the action will attempt to be discerned from this routes.js file.
 
-- routes remain optional
 - actions defined in params directly 'action=theAction' or hitting the named URL for an action '/api/theAction' will always override RESTful routing
 - you can mix explicitly defined params with route-defined params.  If there is an overlap, the route-defined params win
   - IE: /api/user/123?userId=456 => 'connection.userId = 123'
