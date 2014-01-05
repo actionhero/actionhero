3197a800ab9684a33591911e407117b21412f38d
Evan Tahler <evan@taskrabbit.com>
2014-01-04 20:16:30 -0800

    remove grunt test helpers; they are not needed

--- gruntfile.js
+++ gruntfile.js
@@ -17,22 +17,6 @@ grunt.initConfig({
       src: 'public/javascript/actionHeroWebSocket.min.js'
     }
   },
-  env: {
-    test: {
-      NODE_ENV: 'test'
-    },
-    fakeredis: {
-      fakeredis: 'true'
-    },
-    realredis: {
-      fakeredis: 'false'
-    }
-  },
-  mochaTest: {
-    test: {
-      src: 'test/*.js'
-    }
-  },
   projectUpdate: {
     update: {}
   },
@@ -53,12 +37,7 @@ grunt.loadTasks(__dirname + '/grunt')
 grunt.loadNpmTasks('grunt-contrib-clean')
 grunt.loadNpmTasks('grunt-contrib-jshint')
 grunt.loadNpmTasks('grunt-contrib-uglify')
-grunt.loadNpmTasks('grunt-env')
-grunt.loadNpmTasks('grunt-mocha-test')
 grunt.loadNpmTasks('grunt-project-update')
-
-grunt.registerTask('testProduction',['env:test','env:realredis','mochaTest'])
-grunt.registerTask('test',['env:test','env:fakeredis','mochaTest'])
 
 grunt.registerTask('publish',['clean:publish','uglify:publish'])
 grunt.registerTask('update',['projectUpdate'])
