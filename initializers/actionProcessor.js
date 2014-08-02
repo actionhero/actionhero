928726dfb63bafd955747b6155aee3cefe82c6f4
Evan Tahler <evan@taskrabbit.com>
2014-08-02 14:02:58 -0700

    set self.connection._original_connection after nexTick

--- initializers/actionProcessor.js
+++ initializers/actionProcessor.js
@@ -71,16 +71,16 @@ var actionProcessor = function(api, next){
     api.stats.increment('actions:actionsCurrentlyProcessing', -1);
     self.duration = new Date().getTime() - self.actionStartTime;
 
-    self.connection._original_connection.action = self.connection.action;
-    self.connection._original_connection.actionStatus = status;
-    self.connection._original_connection.error = self.connection.error;
-    self.connection._original_connection.response = self.connection.response || {};
+    process.nextTick(function(){
+      self.connection._original_connection.action = self.connection.action;
+      self.connection._original_connection.actionStatus = status;
+      self.connection._original_connection.error = self.connection.error;
+      self.connection._original_connection.response = self.connection.response || {};
 
-    if(typeof self.callback == 'function'){
-      process.nextTick(function(){
+      if(typeof self.callback == 'function'){
         self.callback(self.connection._original_connection, toRender, self.messageCount);
-      });
-    }
+      }
+    });
 
     var logLevel = 'info';
     if(self.actionTemplate != null && self.actionTemplate.logLevel != null){
