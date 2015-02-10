e7b6b8f221d295a4e36de55404ef916dfc72a7a3
Evan Tahler <evan@taskrabbit.com>
2015-02-10 14:26:02 -0800

    better redis config

--- config/redis.js
+++ config/redis.js
@@ -5,30 +5,37 @@
 
 exports.default = { 
   redis: function(api){
-    return {
+    var redisDetails = {
       // Which channel to use on redis pub/sub for RPC communication
       channel: 'actionhero',
       // How long to wait for an RPC call before considering it a failure 
       rpcTimeout: 5000, 
+      // which redis package should you ise?
+      package: 'fakeredis'
+    }
 
-      package: 'fakeredis',
+    if( process.env.FAKEREDIS === 'false' || process.env.REDIS_HOST !== undefined ){
+      // You can opt to use a real redis DB
+      // This is required for multi-server deployments
 
-      // package: 'redis',
-      // host: '127.0.0.1',
-      // port: 6379,
-      // password: null,
-      // options: null,
-      // database: 0
+      redisDetails.package  = 'redis';
+      redisDetails.host     = process.env.REDIS_HOST || '127.0.0.1';
+      redisDetails.port     = process.env.REDIS_PORT || 6379;
+      redisDetails.password = process.env.REDIS_PASS || null;
+      redisDetails.database = process.env.REDIS_DB   || 0;
+      redisDetails.options  = null;
 
-      // package: 'redis-sentinel-client',
-      // port: 26379,
-      // host: '127.0.0.1',
-      // database: 0,
-      // options: {
+      // redisDetails.package  = 'redis-sentinel-client';
+      // redisDetails.port     =  26379;
+      // redisDetails.host     = '127.0.0.1';
+      // redisDetails.database = 0;
+      // redisDetails.options  = {
       //   master_auth_pass: null,
       //   masterName: 'BUS',
-      // }
+      // };
     }
+
+    return redisDetails;
   }
 }
 
