7e9e00493b3d902a88eb260a820eb54880910260
lorenzofox3 <laurent34azerty@gmail.com>
2015-10-20 11:44:23 +0700

    test(staticFile): add spec for last-modified http headers

--- test/core/staticFile.js
+++ test/core/staticFile.js
@@ -1,13 +1,16 @@
 var should = require('should');
+var request = require('request');
 var actionheroPrototype = require(__dirname + '/../../actionhero.js').actionheroPrototype;
 var actionhero = new actionheroPrototype();
 var api;
+var url;
 
-describe('Core: Static File', function(){
+describe.only('Core: Static File', function(){
 
   before(function(done){
     actionhero.start(function(err, a){
       api = a;
+      url = 'http://localhost:' + api.config.servers.web.port+'/'+api.config.servers.web.urlPathForFiles;
       done();
     })
   });
@@ -52,18 +55,33 @@ describe('Core: Static File', function(){
   });
 
 
-  it('should send back the last modified tiem', function (done) {
-    api.specHelper.getStaticFile('simple.html', function (response, connection) {
+  it('should send back the last modified time', function (done) {
+    request.get(url+'/simple.html', function (err, response, body) {
+      response.statusCode.should.eql(200);
+      response.headers['last-modified'].should.be.ok;
       done();
-    })
+    });
   });
 
   it('should send back a 304 if the header "if-modified-since" is present and condition matches', function (done) {
-    done();
+    request.get(url+'/simple.html', function (err, response, body) {
+      response.statusCode.should.eql(200);
+      request({url:url+'/simple.html',headers:{'If-Modified-Since':new Date(Date.now())}}, function (errBis, responseBis, body) {
+        responseBis.statusCode.should.eql(304);
+        done();
+      });
+    });
   });
 
   it('should send back the file if the header "if-modified-since" is present but condition does not match', function (done) {
-    done()
+    request.get(url+'/simple.html', function (err, response, body) {
+      response.statusCode.should.eql(200);
+      var lastModified=new Date(response.headers['last-modified']);
+      request({url:url+'/simple.html',headers:{'If-Modified-Since':new Date(lastModified.getTime()-24*1000*3600)}}, function (errBis, responseBis, body) {
+        responseBis.statusCode.should.eql(200);
+        done();
+      });
+    });
   });
 
 });
