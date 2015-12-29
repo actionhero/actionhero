1c983bf8a71a51f6e8560418f7e14628fd44fa19
Evan Tahler <evantahler@gmail.com>
2015-12-28 17:19:31 -0800

    enable `matchTrailingPathParts`

--- initializers/routes.js
+++ initializers/routes.js
@@ -4,7 +4,6 @@ module.exports = {
 
     api.routes = {};
     api.routes.routes = {};
-
     api.routes.verbs = ['get', 'post', 'put', 'patch', 'delete'];
 
     ////////////////////////////////////////////////////////////////////////////
@@ -12,15 +11,15 @@ module.exports = {
     api.routes.processRoute = function(connection, pathParts){
       if(connection.params.action === undefined || api.actions.actions[connection.params.action] === undefined){
         var method = connection.rawConnection.method.toLowerCase();
-        if(method === 'head'){ method = 'get'; }
+        if(method === 'head' && !api.routes.routes.head){ method = 'get'; }
         for(var i in api.routes.routes[method]){
           var route = api.routes.routes[method][i];
-          var match = api.routes.matchURL(pathParts, route.path);
+          var match = api.routes.matchURL(pathParts, route.path, route.matchTrailingPathParts);
           if(match.match === true){
             if(route.apiVersion){
               connection.params.apiVersion = connection.params.apiVersion || route.apiVersion;
             }
-            
+
             for(var param in match.params){
               try{
                 var decodedName = decodeURIComponent(param.replace(/\+/g, ' '));
@@ -37,8 +36,8 @@ module.exports = {
       }
     }
 
-    api.routes.matchURL = function(pathParts, match){
-      var response = {match: false, params: {} }
+    api.routes.matchURL = function(pathParts, match, matchTrailingPathParts){
+      var response = { match: false, params: {} };
       var matchParts = match.split('/');
       var regexp = '';
       var variable = '';
@@ -46,28 +45,40 @@ module.exports = {
       if(matchParts[0] === ''){ matchParts.splice(0, 1) }
       if(matchParts[(matchParts.length - 1)] === ''){ matchParts.pop() }
 
-      if(matchParts.length !== pathParts.length){
+      if(matchParts.length !== pathParts.length && matchTrailingPathParts !== true){
         return response;
       }
 
       for(var i in matchParts){
-        var part = matchParts[i];
-        if(!pathParts[i]){
+        var matchPart = matchParts[i];
+        var pathPart  = pathParts[i];
+
+        if(matchTrailingPathParts === true){
+          for(var j in pathParts){
+            if(j > i){ pathPart = pathPart + '/' + pathParts[j]; }
+          }
+        }
+
+        if(!pathPart){
           return response;
-        } else if(part[0] === ':' && part.indexOf('(') < 0){
-          variable = part.replace(':', '');
-          response.params[variable] = pathParts[i];
-        } else if(part[0] === ':' && part.indexOf('(') >= 0){
-          variable = part.replace(':', '').split('(')[0];
-          regexp = part.substring(part.indexOf('(') + 1, part.length - 1);
-          var matches = pathParts[i].match(new RegExp(regexp, 'g'));
+        } else if(matchPart[0] === ':' && matchPart.indexOf('(') < 0){
+          variable = matchPart.replace(':', '');
+          response.params[variable] = pathPart
+        } else if(matchPart[0] === ':' && matchPart.indexOf('(') >= 0){
+          variable = matchPart.replace(':', '').split('(')[0];
+          regexp = matchPart.substring(matchPart.indexOf('(') + 1, matchPart.length - 1);
+          var matches = pathPart.match(new RegExp(regexp, 'g'));
           if(matches){
-            response.params[variable] = pathParts[i];
+            response.params[variable] = pathPart;
           } else {
             return response;
           }
         } else {
-          if(pathParts[i] === null || pathParts[i] === undefined || pathParts[i].toLowerCase() !== matchParts[i].toLowerCase()){
+          if(
+            pathPart === null ||
+            pathPart === undefined ||
+            pathParts[i].toLowerCase() !== matchPart.toLowerCase()
+          ){
             return response;
           }
         }
@@ -77,10 +88,17 @@ module.exports = {
       return response;
     }
 
-    // don't ever remove this! 
+    // don't ever remove this!
     // this is really handy for plugins
-    api.routes.registerRoute = function(method, path, action, apiVersion) {
-      api.routes.routes[method].push({ path: path, action: action, apiVersion:apiVersion});
+    api.routes.registerRoute = function(method, path, action, apiVersion, matchTrailingPathParts){
+      if(!matchTrailingPathParts){ matchTrailingPathParts = false; }
+
+      api.routes.routes[method].push({
+        path: path,
+        matchTrailingPathParts: matchTrailingPathParts,
+        action: action,
+        apiVersion: apiVersion
+      });
     }
 
     // load in the routes file
@@ -103,10 +121,10 @@ module.exports = {
           if(method === 'all'){
             for(v in api.routes.verbs){
               verb = api.routes.verbs[v];
-              api.routes.registerRoute(verb, route.path, route.action, route.apiVersion);
+              api.routes.registerRoute(verb, route.path, route.action, route.apiVersion, route.matchTrailingPathParts);
             }
-          } else {
-            api.routes.registerRoute(method, route.path, route.action, route.apiVersion);
+          }else{
+            api.routes.registerRoute(method, route.path, route.action, route.apiVersion, route.matchTrailingPathParts);
           }
           counter++;
         }
