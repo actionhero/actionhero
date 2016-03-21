4977b1a31e7e5e7a8f8c44342701fc1f683c0f64
Evan Tahler <evantahler@gmail.com>
2016-03-21 13:30:19 -0700

    fix localization init update i18n

--- initializers/i18n.js
+++ initializers/i18n.js
@@ -1,10 +1,11 @@
 var i18n = require('i18n');
+var path = require('path');
 
 module.exports = {
   loadPriority:  1,
   initialize: function(api, next){
     api.i18n = {
-      i18n: i18n,
+      // i18n: i18n,
 
       // simplistic determination of locale for connection
       determineConnectionLocale: function(connection){
@@ -24,13 +25,12 @@ module.exports = {
     };
 
     var options = api.config.i18n;
-    options.directory = api.config.general.paths.locale[0];
+    options.directory = path.normalize(api.config.general.paths.locale[0]);
     i18n.configure(options);
-    i18n.setLocale(api.config.i18n.serverLocale);
+    i18n.setLocale(api.config.i18n.defaultLocale);
 
     api.i18n.i18n = i18n;
 
     next();
   }
 };
-
