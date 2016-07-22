b299b8feb89c80c6d7170005f53f208bbbb7f337
Evan Tahler <evantahler@gmail.com>
2016-07-22 13:12:43 -0700

    spec helper should have fingerprint

--- initializers/specHelper.js
+++ initializers/specHelper.js
@@ -109,6 +109,7 @@ module.exports = {
         var id = uuid.v4();
         api.servers.servers.testServer.buildConnection({
           id             : id,
+          fingerprint    : id,
           rawConnection  : {},
           remoteAddress  : 'testServer',
           remotePort     : 0
