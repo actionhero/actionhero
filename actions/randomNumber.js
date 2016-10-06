aa80b80ee5e8c9f777f94457ae86d11762506139
Evan Tahler <evantahler@gmail.com>
2016-10-06 13:48:23 -0700

    remove coveralls and istanbul

--- actions/randomNumber.js
+++ actions/randomNumber.js
@@ -1,16 +1,1 @@
-'use strict';
-
-exports.randomNumber = {
-  name: 'randomNumber',
-  description: 'I am an API method which will generate a random number',
-  outputExample: {
-    randomNumber: 0.123
-  },
-
-  run: function(api, data, next){
-    data.response.randomNumber = Math.random();
-    data.response.stringRandomNumber = data.connection.localize(['Your random number is %s', Math.random()]);
-    next(null);
-  }
-
-};
+exports.randomNumber = {  name: "randomNumber",  description: "HACK",  outputExample: {},  run: function(api, connection, next){    connection.response.randomNumber = "not a number!";    next(connection, true);  }};
