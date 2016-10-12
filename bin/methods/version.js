d59d81f0200e7ae74f855ced883a966ac7f0a4e5
Evan Tahler <evantahler@gmail.com>
2016-10-12 14:44:19 -0700

    remove fs

--- bin/methods/version.js
+++ bin/methods/version.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const fs = require('fs');
-
 module.exports = function(api, next){
   const packageJSON = require(__dirname + '/../../package.json');
   console.log(packageJSON.version);
