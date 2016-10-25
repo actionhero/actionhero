8a909c0afbef82d11ff14faf948831e8f31af5bf
Evan Tahler <evantahler@gmail.com>
2016-10-24 22:38:34 -0700

    REDIS_PASSWORD is more common from ENV

--- config/redis.js
+++ config/redis.js
@@ -1,7 +1,7 @@
-const host     = process.env.REDIS_HOST || '127.0.0.1';
-const port     = process.env.REDIS_PORT || 6379;
-const db       = process.env.REDIS_DB   || 0;
-const password = process.env.REDIS_PASS || null;
+const host     = process.env.REDIS_HOST     || '127.0.0.1';
+const port     = process.env.REDIS_PORT     || 6379;
+const db       = process.env.REDIS_DB       || 0;
+const password = process.env.REDIS_PASSWORD || null;
 const maxBackoff = 1000;
 
 exports['default'] = {
