e0d293953e9c069fc7b796a9a49063aeedf5332f
Evan Tahler <evantahler@gmail.com>
2017-06-12 16:39:56 -0700

    fixup cookie/header test to not hack http core response

--- test/integration/shared_fingerprint.js
+++ test/integration/shared_fingerprint.js
@@ -1,5 +1,6 @@
 var chai = require('chai')
 var dirtyChai = require('dirty-chai')
+var _Primus = require('primus')
 var expect = chai.expect
 chai.use(dirtyChai)
 
@@ -9,22 +10,20 @@ var ActionheroPrototype = require(path.join(__dirname, '/../../actionhero.js'))
 var actionhero = new ActionheroPrototype()
 var api
 
-var client
 var fingerprint
 var url
-var cookieHeader
-var oldRequest
 
-var connectClient = (callback) => {
+var connectClient = (transportOptions, callback) => {
   // get actionheroClient in scope
   eval(api.servers.servers.websocket.compileActionheroClientJS()) // eslint-disable-line
 
-  var S = api.servers.servers.websocket.server.Socket
-  var url = 'http://localhost:' + api.config.servers.web.port
-  var clientSocket = new S(url)
+  var S = _Primus.createSocket()
+  var clientSocket = new S('http://localhost:' + api.config.servers.web.port, {transport: transportOptions})
 
-  client = new ActionheroClient({}, clientSocket) // eslint-disable-line
-  setTimeout(callback, 100)
+  let client = new ActionheroClient({}, clientSocket) // eslint-disable-line
+  setTimeout(() => {
+    callback(null, client)
+  }, 100)
 }
 
 describe('Integration: Web Server + Websocket Socket shared fingerprint', () => {
@@ -42,72 +41,56 @@ describe('Integration: Web Server + Websocket Socket shared fingerprint', () => 
     actionhero.stop(done)
   })
 
-  beforeEach((done) => {
-    cookieHeader = ''
-    connectClient(done)
-  })
-
-  afterEach(() => {
-    try {
-      client.disconnect()
-    } catch (e) {
-      //
-    }
-  })
-
-  before(() => {
-    // Override http.request to test fingerprint
-    var module = require('http')
-    oldRequest = module.request
-    module.request = function (options, callback) {
-      options.headers.Cookie = cookieHeader
-      return oldRequest.apply(module, arguments)
-    }
-  })
-
-  after(() => {
-    // Restore http.request
-    var module = require('http')
-    module.request = oldRequest
-  })
-
   it('should exist when web server been called', (done) => {
     request.get(url + '/api/', (error, response, body) => {
       expect(error).to.be.null()
       body = JSON.parse(body)
       fingerprint = body.requesterInformation.fingerprint
-      cookieHeader = response.headers['set-cookie'][0]
-      client.connect((error, response) => {
+      var headers = { cookie: api.config.servers.web.fingerprintOptions.cookieKey + '=' + fingerprint }
+
+      connectClient({headers: headers}, (error, client) => {
         expect(error).to.be.null()
-        expect(response.status).to.equal('OK')
-        expect(response.data.id).to.be.ok()
-        var id = response.data.id
-        expect(api.connections.connections[id].fingerprint).to.equal(fingerprint)
-        done()
+        client.connect((error, response) => {
+          expect(error).to.be.null()
+          expect(response.status).to.equal('OK')
+          expect(response.data.id).to.be.ok()
+          var id = response.data.id
+          expect(api.connections.connections[id].fingerprint).to.equal(fingerprint)
+          client.disconnect()
+          done()
+        })
       })
     })
   })
 
   it('should not exist when web server has not been called', (done) => {
-    client.connect((error, response) => {
+    connectClient({}, (error, client) => {
       expect(error).to.be.null()
-      expect(response.status).to.equal('OK')
-      expect(response.data.id).to.be.ok()
-      var id = response.data.id
-      expect(api.connections.connections[id].fingerprint).not.to.equal(fingerprint)
-      done()
+      client.connect((error, response) => {
+        expect(error).to.be.null()
+        expect(response.status).to.equal('OK')
+        expect(response.data.id).to.be.ok()
+        var id = response.data.id
+        expect(api.connections.connections[id].fingerprint).not.to.equal(fingerprint)
+        client.disconnect()
+        done()
+      })
     })
   })
 
   it('should exist as long as cookie is passed', (done) => {
-    cookieHeader = api.config.servers.web.fingerprintOptions.cookieKey + '=dummyValue'
-    client.connect((error, response) => {
+    var headers = { cookie: api.config.servers.web.fingerprintOptions.cookieKey + '=' + 'dummyValue' }
+    connectClient({headers: headers}, (error, client) => {
       expect(error).to.be.null()
-      expect(response.status).to.equal('OK')
-      expect(response.data.id).to.be.ok()
-      var id = response.data.id
-      expect(api.connections.connections[id].fingerprint).to.equal('dummyValue')
-      done()
+      client.connect((error, response) => {
+        expect(error).to.be.null()
+        expect(response.status).to.equal('OK')
+        expect(response.data.id).to.be.ok()
+        var id = response.data.id
+        expect(api.connections.connections[id].fingerprint).to.equal('dummyValue')
+        client.disconnect()
+        done()
+      })
     })
   })
 })
