815a59081c60c7c967a5f311f7cf37db02fa6013
Greg Coonrod <greg.coonrod@gmail.com>
2017-09-02 11:51:22 -0500

    Updated task tests with promises.

--- test/tasks/runAction.js
+++ test/tasks/runAction.js
@@ -11,26 +11,12 @@ const actionhero = new ActionheroPrototype()
 let api
 
 describe('Test: RunAction', () => {
-  before((done) => {
-    actionhero.start((error, a) => {
-      expect(error).to.be.null()
-      api = a
-      done()
-    })
-  })
+  before(async () => { api = await actionhero.start() })
+  after(async () => { await actionhero.stop() })
 
-  after((done) => {
-    actionhero.stop(() => {
-      done()
-    })
-  })
-
-  it('can run the task manually', (done) => {
-    api.specHelper.runTask('runAction', {action: 'randomNumber'}, (error, response) => {
-      expect(error).to.not.exist()
-      expect(response.randomNumber).to.be.at.least(0)
-      expect(response.randomNumber).to.be.at.most(1)
-      done()
-    })
+  it('can run the task manually', async () => {
+    const {randomNumber} = await api.specHelper.runTask('runAction', {action: 'randomNumber'})
+    expect(randomNumber).to.be.at.least(0)
+    expect(randomNumber).to.be.at.most(1)
   })
 })
