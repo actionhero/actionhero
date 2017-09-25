ab49a4a99405424f8d73491a76581c170d730b7c
Evan Tahler <evantahler@gmail.com>
2017-09-24 17:43:01 -0700

    check in parser for REDIS_URL

--- config/redis.js
+++ config/redis.js
@@ -1,8 +1,14 @@
-const host = process.env.REDIS_HOST || '127.0.0.1'
-const port = process.env.REDIS_PORT || 6379
-const db = process.env.REDIS_DB || 0
-const password = process.env.REDIS_PASSWORD || null
+let host = process.env.REDIS_HOST || '127.0.0.1'
+let port = process.env.REDIS_PORT || 6379
+let db = process.env.REDIS_DB || 0
+let password = process.env.REDIS_PASSWORD || null
 const maxBackoff = 1000
+
+if (process.env.REDIS_URL) {
+  password = process.env.REDIS_URL.match(/redis:\/\/.*:(.*)@.*:\d*$/i)[1]
+  host = process.env.REDIS_URL.match(/redis:\/\/.*:.*@(.*):\d*$/i)[1]
+  port = parseInt(process.env.REDIS_URL.match(/redis:\/\/.*:.*@.*:(\d*)$/i)[1])
+}
 
 exports['default'] = {
   redis: (api) => {
