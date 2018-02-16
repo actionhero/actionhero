b0137843fb0c16018da34d8ac536ca1a2dd65488
Alim Vedat S <alimvedats@gmail.com>
2018-02-16 03:32:56 +0300

    added: api.utils.parseHeaderForClientAddress(headers)

--- initializers/utils.js
+++ initializers/utils.js
@@ -238,6 +238,41 @@ let responses = await api.utils.asyncWaterfall(jobs)
     }
 
     /**
+     * Return ip and port information if defined in the header
+     *
+     * @param  {Object} headers key-value header parameters
+     * @return {Object} A new Object, contains ip and port
+     */
+    api.utils.parseHeaderForClientAddress = (headers) => {
+      let ip = null
+      let port = null
+
+      if (headers['x-forwarded-port']) {
+        port = headers['x-forwarded-port']
+      }
+
+      if (headers['x-real-ip']) {
+        ip = headers['x-real-ip']
+      } else if (headers['x-forwarded-for']) {
+        let parts
+        let forwardedIp = headers['x-forwarded-for'].split(',')[0]
+        if (forwardedIp.indexOf('.') >= 0 || (forwardedIp.indexOf('.') < 0 && forwardedIp.indexOf(':') < 0)) {
+          // IPv4
+          forwardedIp = forwardedIp.replace('::ffff:', '') // remove any IPv6 information, ie: '::ffff:127.0.0.1'
+          parts = forwardedIp.split(':')
+          if (parts[0]) { ip = parts[0] }
+          if (parts[1]) { port = parts[1] }
+        } else {
+          // IPv6
+          parts = api.utils.parseIPv6URI(forwardedIp)
+          if (parts.host) { ip = parts.host }
+          if (parts.port) { port = parts.port }
+        }
+      }
+      return { ip, port }
+    }
+
+    /**
      * Transform the cookie headers of a node HTTP `req` Object into a hash.
      *
      * @param  {Object} req A node.js `req` Object
