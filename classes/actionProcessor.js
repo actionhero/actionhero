174138139967891494a18d8e69bdce871d8b2369
Evan Tahler <evan.tahler@voom.flights>
2018-03-07 16:47:21 -0800

    passthough api.config.errors.genericError if missing

--- classes/actionProcessor.js
+++ classes/actionProcessor.js
@@ -46,7 +46,7 @@ module.exports = class ActionProcessor {
     this.actionStatus = String(status)
 
     if (status instanceof Error) {
-      error = await api.config.errors.genericError(this, status)
+      error = (typeof api.config.errors.genericError === 'function' ? await api.config.errors.genericError(this, status) : status)
     } else if (status === 'server_shutting_down') {
       error = await api.config.errors.serverShuttingDown(this)
     } else if (status === 'too_many_requests') {
