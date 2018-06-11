658b895b742dc7041f062b5c0f33cab7169280c6
Eddie Torrejon <etorrejon@fastmail.fm>
2018-06-11 11:51:57 -0400

    Adding check for log configuration

--- config/logger.js
+++ config/logger.js
@@ -18,15 +18,19 @@ exports['default'] = {
     }
 
     // file logger
-    logger.transports.push(function (api, winston) {
-      const logDirectory = api.config.general.paths.log[0]
+    const hasLogDirectoryConfigured = (api.config.general.paths.log.length === 1)
 
-      return new (winston.transports.File)({
-        filename: logDirectory + '/' + api.pids.title + '.log',
-        level: 'info',
-        timestamp: function () { return api.id + ' @ ' + new Date().toISOString() }
+    if (hasLogDirectoryConfigured) {
+      logger.transports.push(function (api, winston) {
+        const logDirectory = api.config.general.paths.log[0]
+
+        return new (winston.transports.File)({
+          filename: logDirectory + '/' + api.pids.title + '.log',
+          level: 'info',
+          timestamp: function () { return api.id + ' @ ' + new Date().toISOString() }
+        })
       })
-    })
+    }
 
     // the maximum length of param to log (we will truncate)
     logger.maxLogStringLength = 100
@@ -46,17 +50,21 @@ exports.test = {
     let logger = { transports: [] }
 
     // file logger
-    logger.transports.push(function (api, winston) {
-      const logDirectory = api.config.general.paths.log[0]
+    const hasLogDirectoryConfigured = (api.config.general.paths.log.length === 1)
 
-      return new (winston.transports.File)({
-        filename: logDirectory + '/' + api.pids.title + '.log',
-        maxsize: 20480,
-        maxFiles: 1,
-        level: 'debug',
-        timestamp: function () { return api.id + ' @ ' + new Date().toISOString() }
+    if (hasLogDirectoryConfigured) {
+      logger.transports.push(function (api, winston) {
+        const logDirectory = api.config.general.paths.log[0]
+
+        return new (winston.transports.File)({
+          filename: logDirectory + '/' + api.pids.title + '.log',
+          maxsize: 20480,
+          maxFiles: 1,
+          level: 'debug',
+          timestamp: function () { return api.id + ' @ ' + new Date().toISOString() }
+        })
       })
-    })
+    }
 
     return logger
   }
