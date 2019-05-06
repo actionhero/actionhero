fc8a77bc4c91acb75b6eedd0d0df66c224976e2f
Chad Robinson <crobinson@medialantern.com>
2019-05-06 11:37:40 -0600

    Allow custom errors to specify response HTTP codes
    Currently if you throw an error during any action, the response code is always 400. This change allows you to use the following pattern:
    
    ```
    function NotFoundError(message, extraFields) {
        Error.call(this);
    
        this.name = this.constructor.name;
        this.message = message || '404: Not Found';
        this.responseHttpCode = 404;
        Object.assign(this, extraFields || {});
    }
    ```
    
    then in an action:
    
    ```
    const { NotFoundError } = require('../../lib/customErrors');
    ...
    const user = api.models.User.findByPk(userId);
    if (!user) {
        throw new NotFoundError('Invalid userId');
    }
    ```
    
    This will result in a "Invalid userId" message passed back, with a responseHttpCode of 404 (not found). You can define as many error types as you want this way, e.g. using `SessionError/AuthError` for indicating 401/403 responses, `ServerError` for 5xx series errors (remote database failed), etc.

--- classes/actionProcessor.js
+++ classes/actionProcessor.js
@@ -98,6 +98,11 @@ module.exports = class ActionProcessor {
     }
 
     if (error) {
+      // If the error is a custom error that specifies a response code, pass that through
+      if (error.responseHttpCode && this.connection.rawConnection) {
+        this.connection.rawConnection.responseHttpCode = error.responseHttpCode;
+      }
+
       if (error instanceof Error) {
         logLine.error = String(error)
       } else {
