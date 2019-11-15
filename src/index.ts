6073644a315688339066dbd588a3769782f9d413
Evan Tahler <evan@evantahler.com>
2019-11-15 11:46:24 -0800

    comments and commas

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,4 @@
-// import * as path from "path";
-// import * as fs from "fs";
 import { Api } from "./classes/api";
-
-// function requireRelative(klass: string, file: string) {
-//   let fullPath = path.join(
-//     process.cwd(),
-//     "node_modules",
-//     "actionhero",
-//     "classes",
-//     file
-//   );
-//   if (!fs.existsSync(fullPath)) {
-//     fullPath = path.join(__dirname, "classes", file);
-//   }
-
-//   exports[klass] = require(fullPath)[klass];
-// }
-
-// [
-//   { klass: "Process", file: "process" },
-//   { klass: "Action", file: "action" },
-//   { klass: "Task", file: "task" },
-//   { klass: "Initializer", file: "initializer" },
-//   { klass: "Server", file: "server" },
-//   { klass: "CLI", file: "cli" },
-//   { klass: "ActionProcessor", file: "actionProcessor" },
-//   { klass: "Connection", file: "connection" }
-// ].forEach(({ klass, file }) => {
-//   requireRelative(klass, file);
-// });
-
 export { Process } from "./classes/process";
 export { Initializer } from "./classes/initializer";
 export { Connection } from "./classes/connection";
