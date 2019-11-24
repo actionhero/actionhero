0b7c55cad99563d0f51ba09c292d3ab9cfc95670
Evan Tahler <evan@evantahler.com>
2019-11-23 23:24:00 -0800

    more elaborate ts mode chcek when running ts-node-dev

--- src/classes/process/typescript.ts
+++ src/classes/process/typescript.ts
@@ -1,8 +1,16 @@
 import * as path from "path";
 
 function isTypescript(): boolean {
+  // if this file is typescript, we are running typescript :D
+  // this is the best check, but fails when actionhero is compiled to js though...
   const extension = path.extname(__filename);
   if (extension === ".ts") {
+    return true;
+  }
+
+  // are we running via a ts-node/ts-node-dev shim?
+  const lastArg = process.execArgv[process.execArgv.length - 1];
+  if (lastArg && path.parse(lastArg).name.indexOf("ts-node") > 0) {
     return true;
   }
 
