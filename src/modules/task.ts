33d5db45214cd2273a1d02b2e6a228d5ac38fcf6
a.kuzmenko <1782530+witem@users.noreply.github.com>
2020-07-07 18:48:16 +0300

    fix-task-data-type | change types in task module
    Signed-off-by: a.kuzmenko <1782530+witem@users.noreply.github.com>

--- src/modules/task.ts
+++ src/modules/task.ts
@@ -1,6 +1,6 @@
 import { api, config, utils, log } from "./../index";
 import { Inputs } from "./../classes/inputs";
-import { Task } from "./../classes/task";
+import { Task, TaskData } from "./../classes/task";
 
 export namespace task {
   /**
@@ -55,7 +55,7 @@ export namespace task {
    */
   export async function enqueue(
     taskName: string,
-    inputs: { [key: string]: any },
+    inputs: TaskData,
     queue: string = api.tasks.tasks[taskName].queue
   ) {
     await validateInput(taskName, inputs);
@@ -74,7 +74,7 @@ export namespace task {
   export async function enqueueAt(
     timestamp: number,
     taskName: string,
-    inputs: { [key: string]: any },
+    inputs: TaskData,
     queue: string = api.tasks.tasks[taskName].queue
   ) {
     await validateInput(taskName, inputs);
@@ -94,7 +94,7 @@ export namespace task {
   export async function enqueueIn(
     time: number,
     taskName: string,
-    inputs: { [key: string]: any },
+    inputs: TaskData,
     queue: string = api.tasks.tasks[taskName].queue
   ) {
     await validateInput(taskName, inputs);
@@ -114,7 +114,7 @@ export namespace task {
   export async function del(
     q: string,
     taskName: string,
-    args?: { [key: string]: any },
+    args?: TaskData,
     count?: number
   ) {
     return api.resque.queue.del(q, taskName, [args], count);
@@ -132,7 +132,7 @@ export namespace task {
   export async function delDelayed(
     q: string,
     taskName: string,
-    inputs?: { [key: string]: any }
+    inputs?: TaskData
   ) {
     return api.resque.queue.delDelayed(q, taskName, [inputs]);
   }
@@ -149,7 +149,7 @@ export namespace task {
   export async function scheduledAt(
     q: string,
     taskName: string,
-    inputs: { [key: string]: any }
+    inputs: TaskData
   ): Promise<Array<number>> {
     return api.resque.queue.scheduledAt(q, taskName, [inputs]);
   }
@@ -175,7 +175,7 @@ export namespace task {
     q: string,
     start: number,
     stop: number
-  ): Promise<Array<{ [key: string]: any }>> {
+  ): Promise<Array<TaskData>> {
     return api.resque.queue.queued(q, start, stop);
   }
 
