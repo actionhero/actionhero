2975ce543fb8fd654cec8689f7ee69c15b5eec70
Evan Tahler <evan@delicioushat.com>
2020-08-26 17:09:58 -0700

    Do not 'start' the server in the CLI console (#1574)
    * do not 'start' the server in the CLI console
    
    * do not 'start' the server in the CLI console
    
    * prettier

--- src/bin/methods/console.ts
+++ src/bin/methods/console.ts
@@ -1,5 +1,5 @@
 import * as REPL from "repl";
-import { config, api, env, utils, CLI } from "./../../index";
+import { api, env, CLI } from "./../../index";
 
 export class Console extends CLI {
   constructor() {
@@ -10,18 +10,6 @@ export class Console extends CLI {
   }
 
   async run() {
-    for (const i in config.servers) {
-      config.servers[i].enabled = false;
-    }
-    config.general.developmentMode = false;
-    config.tasks.scheduler = false;
-    config.tasks.queues = [];
-    config.tasks.minTaskProcessors = 0;
-    config.tasks.maxTaskProcessors = 0;
-
-    await api.commands.start.call(api.process);
-    await utils.sleep(500);
-
     await new Promise((resolve, reject) => {
       const repl = REPL.start({
         prompt: "[ AH::" + env + " ] >> ",
