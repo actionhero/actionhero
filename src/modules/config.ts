a3fe864f9739197a705886ea71c073309fa58642
Evan Tahler <evantahler@gmail.com>
2020-10-08 14:15:58 -0700

    path.sep is a better seperator

--- src/modules/config.ts
+++ src/modules/config.ts
@@ -1,7 +1,6 @@
 import * as fs from "fs";
 import * as path from "path";
 import * as glob from "glob";
-import * as os from "os";
 import { argv } from "optimist";
 import { utils } from "./utils";
 import { ensureNoTsHeaderFiles } from "./utils/ensureNoTsHeaderFiles";
@@ -86,7 +85,9 @@ export function buildConfig(_startingParams: ConfigInterface = {}) {
     if (f.includes("routes.js") || f.includes("routes.ts")) {
       // We don't want to merge in routes from Actionhero core unless we are running core directly
       // Routes can be loaded by plugins via `registerRoute`
-      if (f.includes(`${os.EOL}node_modules${os.EOL}actionhero${os.EOL}`)) {
+      if (
+        f.includes(`${path.sep}node_modules${path.sep}actionhero${path.sep}`)
+      ) {
         return;
       }
 
