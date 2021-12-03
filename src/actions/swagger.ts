98291f4d253a6f73fcab3526353778d35bb79aca
Pedro S. Lopez <pslamoros@hotmail.com>
2021-12-03 19:25:18 -0400

    use global flag in swagger path substitution (#2039)
    * use global flag in swagger path substitution
    
    * add tests

--- src/actions/swagger.ts
+++ src/actions/swagger.ts
@@ -79,11 +79,7 @@ export class Swagger extends Action {
         const tag = action.name.split(":")[0];
         const formattedPath = route.path
           .replace("/v:apiVersion", "")
-          .replace(/\/:(\w*)/, "/{$1}")
-          .replace(/\/:(\w*)/, "/{$1}")
-          .replace(/\/:(\w*)/, "/{$1}")
-          .replace(/\/:(\w*)/, "/{$1}")
-          .replace(/\/:(\w*)/, "/{$1}");
+          .replace(/\/:(\w*)/g, "/{$1}");
 
         swaggerPaths[formattedPath] = swaggerPaths[formattedPath] || {};
         swaggerPaths[formattedPath][method] = {
