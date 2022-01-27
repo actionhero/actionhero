d13e75c54eb6fd561b4cd75e8f389c4b13e7b596
Pedro S. Lopez <pedroslopez@me.com>
2022-01-27 18:02:38 -0400

    use commander to handle cli `formatter`/`validator`

---
+++ __tests__/testCliCommands/hello.ts
@@ -0,0 +1,41 @@
+import { CLI, ParamsFrom } from "./../../src/index";
+
+export class HelloCliTest extends CLI {
+  name = "hello";
+  description = "I work";
+  inputs = {
+    name: {
+      required: false,
+      default: "World",
+    },
+    title: {
+      letter: "t",
+      required: true,
+      default: "Dr.",
+      formatter: (val: string) => {
+        return `${val}.`;
+      },
+      validator: (val: string) => {
+        const parts = val.split(".");
+        if (parts.length > 2) throw new Error("too many periods");
+      },
+    },
+    countries: {
+      variadic: true as const,
+      formatter: (val: string) => `${val}!`,
+      validator: (val: string) => {
+        if (val.length > 0 && val[0].toUpperCase() !== val[0])
+          throw new Error("country not capitalized");
+      },
+    },
+  };
+
+  async run({ params }: { params: Partial<ParamsFrom<HelloCliTest>> }) {
+    console.log(
+      `Hello, ${params.title} ${params.name} ${
+        params.countries ? `(${params.countries.join(" ")})` : ""
+      }`
+    );
+    return true;
+  }
+}
