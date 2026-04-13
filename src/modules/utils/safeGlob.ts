0e28a5259fa6abd1e25aa4b2646883275bf151c0
shui <shuizheng123@gmail.com>
2026-04-13 09:38:26 +0800

    fix: use GlobOptions instead of deprecated IOptions for glob package
    The glob package v9+ renamed IOptions to GlobOptions. This fix updates
    the type reference in safeGlob.ts to resolve the TypeScript error.
    
    Closes #3015

--- src/modules/utils/safeGlob.ts
+++ src/modules/utils/safeGlob.ts
@@ -1,6 +1,6 @@
 import * as glob from "glob";
 
-export function safeGlobSync(match: string, args: glob.IOptions = {}) {
+export function safeGlobSync(match: string, args: glob.GlobOptions = {}) {
   const isWindows = process.platform === "win32";
   return glob.sync(match, { ...args, windowsPathsNoEscape: isWindows });
 }
